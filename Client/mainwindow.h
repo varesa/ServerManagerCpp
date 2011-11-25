f9f471eb2288ade49738269dd68ef7f220325a81
Esa Varemo <esa@kuivanto.fi>
2011-11-25 23:42:42 +0200

    Annettu MainWindow():ille dekonstruktori, mihin lie kadonnut? undefined reference to: json_spirit::read()

--- Client/mainwindow.h
+++ Client/mainwindow.h
@@ -24,7 +24,7 @@ class MainWindow : public QMainWindow
 
 public:
     explicit MainWindow(QWidget *parent = 0);
-    ~MainWindow();
+    ~MainWindow() {}
 
 private slots:
     void requestServers();
